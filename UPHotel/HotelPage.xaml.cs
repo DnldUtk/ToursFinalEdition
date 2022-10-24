132fa1ff884e892bdd7122afffceee3b65e3dff4
Zver <Zver@ZVERDVD-GJSRG4K>
2022-10-24 10:18:28 +0800

    Добавьте файлы проекта.

---
+++ UPHotel/HotelPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UPHotel
+{
+    /// <summary>
+    /// Логика взаимодействия для BookingPage.xaml
+    /// </summary>
+    public partial class BookingPage : Page
+    {
+        public BookingPage()
+        {
+            InitializeComponent();
+            //DGridHotels.ItemsSource = UPEntities2.GetContext().Hotels.ToList();
+        }
+
+        private void ButtonEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MngrMainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                UPEntities2.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = UPEntities2.GetContext().Hotels.ToList();
+            }
+        }
+    } 
+}
---
+++ UPHotel/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace UPHotel
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HotelsPage());
+            Manager.MngrMainFrame = MainFrame;
+            //ImportTours();
+        }
+
+        private void ImportTours()
+        {
+            var fileData = File.ReadAllLines(@"C:\Users\Zver\Desktop\Tours.txt");
+            var img = Directory.GetFiles(@"C:\Users\Zver\Desktop\Tours");
+
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+
+                var tempTour = new Tour
+                {
+                    Name = data[0].Replace("\"", ""),
+                    TicketCount = int.Parse(data[2]),
+                    Price = decimal.Parse(data[3]),
+                    IsActual = (data[4] == "0") ? false : true
+                };
+                foreach (var tourType in data[5].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentType = UPEntities2.GetContext().Types.ToList().FirstOrDefault(p => p.Name == tourType);
+                    if (currentType != null)
+                        tempTour.Types.Add(currentType);
+                }
+                try
+                {
+                    tempTour.ImagePreview = File.ReadAllBytes(img.FirstOrDefault(p => p.Contains(tempTour.Name)));
+                }
+                catch (Exception ex)
+
+                { Console.WriteLine(ex.Message); }
+                UPEntities2.GetContext().Tours.Add(tempTour);
+                UPEntities2.GetContext().SaveChanges();
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MngrMainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BackButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BackButton.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
